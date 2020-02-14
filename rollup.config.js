9d573534fe0c6b1ed19da5b591f56544ed57a493
Serhiy Dobrelya <s.dobrelya@eska.global>
2020-02-14 18:49:07 +0200

    start

---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import replace from 'rollup-plugin-replace';
+import postcss from 'rollup-plugin-postcss-modules';
+import autoprefixer from 'autoprefixer'
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+
+export default {
+    input: 'src/index.tsx',
+    output: [
+        {
+            file: 'public/dist/index.js',
+            format: 'iife',
+            sourcemap: true
+        }
+    ],
+    external: [
+        'react',
+        'react-dom',
+    ],
+    treeshake: false,
+    plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        resolve(),
+        external(),
+        postcss({
+            extract: true,
+            plugins: [autoprefixer()]
+        }),
+        url(),
+        svgr(),
+        typescript({
+            rollupCommonJSResolveHack: true,
+            clean: true
+        }),
+        commonjs(),
+        serve({
+            open: true,
+            contentBase: ['public']
+        }),
+        livereload({
+            watch: 'public/dist',
+        }),
+        sizeSnapshot()
+    ]
+}
---
+++ src/List/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './style.css';
+
+interface IListProps {
+}
+
+const List: React.FC<IListProps> = () => {
+
+    return (
+        <ul className={style.root}>
+            <li className={style.listItem}>
+                aaa
+            </li>
+        </ul>
+    )
+};
+
+export default List;
